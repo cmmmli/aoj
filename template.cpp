ad4eca478b1a5b0ac4807ce5ed0862f7080ad5b4
komoriyuichiro <thekomori1113@gmail.com>
2018-11-20 04:54:15 +0900

    initial commit

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <cmath>
+#include <map>
+using namespace std;
+
+typedef long long ll;
+
+int dx[4] = { 0, -1, 1, 0 };
+int dy[4] = { -1, 0, 0, 1 };
+
+int main() {
+  int N;
+  cin >> N;
+
+  return 0;
+}
